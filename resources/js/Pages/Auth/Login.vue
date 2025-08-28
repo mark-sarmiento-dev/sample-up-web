04c90790b7eb403d3021f16d4949bee081ea2cae
jow <jowcueto@mcgi.org>
2025-08-28 17:29:14 +0800

    Login and dashboard layout implementation

---
+++ resources/js/Pages/Auth/Login.vue
@@ -0,0 +1,47 @@
+<template>
+    <main class="login-container">
+        <section class="left" aria-label="Decorative Image">
+        </section>
+        <section class="right">
+            <!-- Logo -->
+            <img src="images/logo.png" alt="Company Logo" class="logo" />
+            <!-- Title -->
+            <h1>
+                LOGIN
+            </h1>
+            <!-- Error Message -->
+            <!-- <div class="error-message">
+            Invalid username or password
+            </div> -->
+            <!-- Form -->
+            <form>
+                <label for="username">
+                    Username
+                </label>
+                <input type="text" id="username" placeholder="Username" required>
+                <label for="password">
+                    Password
+                </label>
+                <input type="password" id="password" placeholder="Password" required>
+                <!-- Forgot Password link -->
+                <div class="forgot-password">
+                    <a href="#">
+                        Forgot Password?
+                    </a>
+                </div>
+                <button type="submit">
+                    Login
+                </button>
+            </form>
+            <!-- Sign up link -->
+            <div class="signup">
+                Don’t have an account?
+                <a href="#">
+                    Sign up
+                </a>
+            </div>
+        </section>
+    </main>
+</template>
+<script>
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', function () {
+    return Inertia::render('Auth/Login');
+})->name('login');
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->name('dashboard');
