86b3f54cb7e127aa60fcf8843ad0a9f2650faabb
jow <jowcueto@mcgi.org>
2025-09-03 21:32:24 +0800

    Create modal components

---
+++ resources/js/Pages/Playground.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="playground-container">
+        <h1>Playground</h1>
+        <div class="playground-content">
+            <h3>Modals</h3>
+            <button class="btn" @click="showModal = true">Open Small Modal</button>
+
+            <Modal v-model="showModal" title="Small Modal" size="sm">
+                <p>This is a small modal.</p>
+            </Modal>
+        </div>
+    </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+import Modal from "@/Components/Modal.vue"
+
+const showModal = ref(false);
+</script>
+<style scoped>
+    .playground-container {
+        margin: 40px;
+    }
+
+    .playground-content {
+        margin-top: 40px;
+    }
+
+    .btn {
+        padding:12px;
+		margin-top:20px;
+		background-color:#5B5B5B;
+		/* Theme color */
+		border:none;
+		color:white;
+		font-size:16px;
+		cursor:pointer;
+		border-radius:0;
+    }
+</style>
